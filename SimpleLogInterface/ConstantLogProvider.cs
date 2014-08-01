2a95ab87f794614393ab394a737ec43f6e0c33c5
Jeff Ashton <jeff.ashton@d2l.com>
2014-08-01 18:09:28 -0400

    Adding a constant log provider.

---
+++ SimpleLogInterface/ConstantLogProvider.cs
@@ -0,0 +1,16 @@
+﻿
+namespace SimpleLogInterface {
+
+	public sealed class ConstantLogProvider : ILogProvider {
+
+		private readonly ILog m_log;
+
+		public ConstantLogProvider( ILog log ) {
+			m_log = log;
+		}
+
+		public ILog Get( string name ) {
+			return m_log;
+		}
+	}
+}
