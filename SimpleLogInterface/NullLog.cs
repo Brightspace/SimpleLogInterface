7ce0b7aa506f2c5729d6235abfa7baefc7bf1ced
Jeff Ashton <jeff.ashton@d2l.com>
2014-08-01 09:54:45 -0400

    Adding null logging implementations.

---
+++ SimpleLogInterface/NullLog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SimpleLogInterface {
+
+	public sealed class NullLog : ILog {
+
+		public static readonly ILog Instance = new NullLog();
+
+		private NullLog() {
+		}
+
+		void ILog.Debug( Func<object> messageBuilder, Exception exception ) {
+		}
+
+		void ILog.Info( Func<object> messageBuilder, Exception exception ) {
+		}
+
+		void ILog.Warn( Func<object> messageBuilder, Exception exception ) {
+		}
+
+		void ILog.Error( Func<object> messageBuilder, Exception exception ) {
+		}
+
+		void ILog.Fatal( Func<object> messageBuilder, Exception exception ) {
+		}
+
+	}
+}
---
+++ SimpleLogInterface/NullLogProvider.cs
@@ -0,0 +1,15 @@
+﻿
+namespace SimpleLogInterface {
+
+	public sealed class NullLogProvider : ILogProvider {
+
+		public static readonly ILogProvider Instance = new NullLogProvider();
+
+		private NullLogProvider() {
+		}
+
+		ILog ILogProvider.Get( string name ) {
+			return NullLog.Instance;
+		}
+	}
+}
