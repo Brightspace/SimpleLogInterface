7f7b008e6b659ef6f23cb644e7c6e17f9ed942dc
Jacob Parker <jacob@solidangle.ca>
2016-02-13 23:17:43 -0500

    Remove build script

--- build/package.bat
+++
@@ -1,6 +0,0 @@
-msbuild ..\SimpleLogInterface.sln /target:Clean
-msbuild ..\SimpleLogInterface.sln /property:Configuration=Release
-..\.nuget\nuget pack ..\SimpleLogInterface\SimpleLogInterface.csproj -Prop Configuration=Release -IncludeReferencedProjects
-..\.nuget\nuget pack ..\SimpleLogInterface.Log4Net\SimpleLogInterface.Log4Net.csproj -Prop Configuration=Release -IncludeReferencedProjects
-..\.nuget\nuget pack ..\SimpleLogInterface.NUnit\SimpleLogInterface.NUnit.csproj -Prop Configuration=Release -IncludeReferencedProjects
-@pause
